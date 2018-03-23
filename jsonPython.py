0f1969951e1fc7579117932079f4216dba1a2a8c
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-03-22 22:31:00 -0700

    learning json

--- jsonPython.py
+++ jsonPython.py
@@ -26,3 +26,25 @@ print jsonFileData['office']['medical']
 print jsonFileData['office']['medical'][0]
 print jsonFileData['office']['medical'][1]
 print jsonFileData['office']['medical'][1]['room-number']
+
+print "------------ Reading a webpage using JSON --------------"
+import urllib2, urllib, json
+baseurl = "https://query.yahooapis.com/v1/public/yql?"
+yql_query = "select wind from weather.forecast where woeid=2460286"
+yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
+print yql_url
+result = urllib2.urlopen(yql_url).read()
+data = json.loads(result)
+print data
+print data['query']['results']
+
+print "------------ Reading a webpage using JSON --------------"
+
+import urllib2
+import json
+req = urllib2.Request("http://vimeo.com/api/v2/video/260920165.json")
+opener = urllib2.build_opener()
+f = opener.open(req)
+json = json.loads(f.read())
+print json
+print json[0]['title']
