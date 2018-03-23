33b0d0d433b9ffb5fd979e0f2151f28a628a107c
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-03-22 22:32:00 -0700

    using MEDIUM.com APIs

---
+++ mediumStories.py
@@ -0,0 +1,30 @@
+import requests , jsonme
+
+# CLIENT_ID = "MY_CLIENT_ID"
+# CLIENT_SECRET = "MY_CLIENT_SECRET"
+# MEDIUM = "https://medium.com"
+
+def clean_json_response(response):
+    return json.loads(response.text.split('])}while(1);</x>')[1])
+
+def latestPostsFromMyFollowingAuthors(usernames):
+    # sample output : https://medium.com/@shreyans.mulkutkar/latest?format=json
+    print('Retrieving the latest posts...')
+    post_ids = []
+    for username in usernames:
+        url = MEDIUM + '/@' + username + '/latest?format=json'
+        print url
+        response = requests.get(url)
+        response_dict = clean_json_response(response)
+        print response_dict
+        try:
+            posts = response_dict['payload']['references']['Post']
+        except:
+            posts = []
+        if posts:
+            print posts
+            for key in posts.keys():
+                post_ids.append(posts[key]['id'])
+    return post_ids
+
+print latestPostsFromMyFollowingAuthors(["shreyans.mulkutkar"])
